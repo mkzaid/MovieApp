ee11870fa5ccce61fa4a3e82e456b732463f8ca8
Zaid Umar <zaidtheprogrammer@gmail.com>
2023-12-08 15:41:39 +0530

    Added Navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./navbar.css"
+const Navbar = () => {
+  return (
+    <div  id='navbar' >
+              <div className='navbar_elements' >
+                    <div id='logo'>
+                      <img   src='' alt='logo' />  </div>
+                    <div id='list'> </div>
+                    <div id='user'> </div>
+              </div>
+    </div>
+  )
+}
+
+export default Navbar   
